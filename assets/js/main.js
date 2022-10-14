58e906d9f39601518521e6b74d5d9cd403336be0
MoSuperCode <mo.supercode@gmail.com>
2022-10-14 14:19:15 +0200

    Abgabe+JS

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+function toggle() {
+    const tag1 = document.getElementById("test1");
+    const tag2 = document.getElementById("test2");
+    const tag3 = document.getElementById("test3");
+    const tag4 = document.getElementById("test4");
+    const tag5 = document.getElementById("test5");
+    const tag6 = document.getElementById("test6");
+    tag1.classList.toggle("toggle")
+    tag2.classList.toggle("toggle2")
+    tag3.classList.toggle("toggle2")
+    tag4.classList.toggle("toggle2")
+    tag5.classList.toggle("toggle2")
+    tag6.classList.toggle("toggle3")
+}
